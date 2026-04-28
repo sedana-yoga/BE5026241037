f077c8a504792c3ba34471059633bb642bc518f8
sedana-yoga <sedanayoga.edu@gmail.com>
2026-04-28 21:47:50 +0700

    pertemuan 7

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>5026241037</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <center>
+            <h1>5026241037 I Gusti Made Sedana Yoga</h1>
+            <p>
+                <a href="pert1" target="blank" class="btn btn-primary">
+                    Pertemuan1-Intro
+                </a>
+            </p>
+            <p>
+                <a href="pert2" target="blank" class="btn btn-primary">
+                    Pertemuan 2 - News</a>
+            </p>
+            <p>
+                <a href="pert2-1" target="blank" class="btn btn-primary">
+                    Pertemuan 2 - News1
+                </a>
+            </p>
+            <p>
+                <a href="pert3" target="blank" class="btn btn-primary">
+                    Pertemuan 3 - Responsive
+                </a>
+            </p>
+            <p>
+                <a href="tpert3" target="blank" class="btn btn-primary">
+                    Tugas Pertemuan 3
+                </a>
+            </p>
+            <p>
+                <a href="tpert4" target="blank" class="btn btn-primary">
+                    Tugas Pertemuan 4
+                </a>
+            </p>
+            <p>
+                <a href="pert5" target="blank" class="btn btn-primary">
+                    Pertemuan 5 - Arsha
+                </a>
+            </p>
+            <p>
+                <a href="tlinktree" target="blank" class="btn btn-primary">
+                    Tugas Linktree
+                </a>
+            </p>
+        </center>
+    </div>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+    return "<h1>Halo, selamat datang</h1> di tutorial laravel <u>www.malasngoding.com</u>";
+});
+
+Route::get('blog', function () {
+    return view('blog');
+});
+
+Route::get('pert1', function () {
+    return view('pertemuan1-intro');
+});
+
+Route::get('pert2', function () {
+    return view('pertemuan2-news');
+});
+
+Route::get('pert2-1', function () {
+    return view('pertemuan2-news1');
+});
+
+Route::get('pert3', function () {
+    return view('pertemuan3-responsive');
+});
+
+Route::get('tpert3', function () {
+    return view('tugaspertemuan3');
+});
+
+Route::get('tpert4', function () {
+    return view('tugaspertemuan4');
+});
+
+Route::get('pert5', function () {
+    return view('pertemuan5-arsha');
+});
+
+Route::get('tlinktree', function () {
+    return view('tugaslinktree');
+});
+
+Route::get('menu', function () {
+    return view('menu');
+});
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('biodata', [DosenController::class, 'biodata']);
